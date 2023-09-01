c845e8c36ec6d90ffebef290035d95192b7a4207
samuel <samugit.projects@gmail.com>
2023-09-01 14:49:36 -0300

    Aula01 Introdução às operações de I/O

---
+++ src/theory/IntroIO.java
@@ -0,0 +1,26 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class IntroIO {
+
+    /*
+    *
+    *  INPUT               OUTPUT
+    * ENTRADA              SAIDA
+    *
+    * ARQUIVO   ->      -> ARQUIVO
+    * TECLADO   -> APP  -> CONSOLE
+    * DOWNLOAD  ->      -> UPLOAD
+    *
+    *               I/O
+    *
+    * JAVA I/O - API antiga (quando Java foi criado)
+    * JAVA NIO - Nova API também antiga (trouxe melhorias há muito tempo)
+    * JAVA NIO.2 - Quando se fala em I/O hoje no Java nos referimos a essa API, que tem ligação com as anteriores
+    *
+    * Um dos principais tipos de I/O que temos é o IO para arquivo (FILE) que consiste em ler os dados, usar na aplicação
+    * e gravar dados dentro de arquivos
+    * Será visto como trabalhar com dados de arquivos em termos de Bytes, que é o tipo mais fundamental de dado
+    * que podemos utilizar para "coisas" em arquivos, como enviar pela internet por exemplo
+    * */
+}
