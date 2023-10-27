debf75caf9dda06ea5d75be06785c506198627a2
Samuel <samugit.projects@gmail.com>
2023-10-27 12:25:58 -0300

    Aula22 Transferir dados entre Input...OutputStreams p1

--- src/inout/IOOperations.java
+++ src/inout/IOOperations.java
@@ -39,4 +39,22 @@ public class IOOperations { // classe utilizada para abstrair os códigos de lei
     public static void write(OutputStream out, byte[] bytes) throws IOException {
         out.write(bytes);
     }
+
+    // acrescentando método para realizar transferencia de arquivos seja qual for (download da web, transferencia para outro pc, etc)
+    // a grande vantagem do uso de Stream é o de seguir lendo, e escrevendo, o que foi lido sem precisar carregar tudo na memória
+    // por isso requer atenção na forma de uso como é o caso do read() que utiliza um readAllBytes() mais acima que num arquivo de 1TB faria a leitura toda para a RAM
+    public static void tranfer(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[2048]; // buffer = area temporária de memória para receber os bytes lidos nesse array de bytes, e transferir para a saída, podendo receber mais informações
+
+        while (true) { // tecnicamente infinito, porém, colocamos a condição para encerrar quando todos os dados forem lidos
+            int bytesRead = in.read(buffer); // para armazenar os dados lidos colocamos no buffer // a partir de 0 até o tamanho de dados que queremos gravar (usando o length)
+
+            if (bytesRead < 0) {
+                break;
+            }
+            out.write(buffer, 0, bytesRead); // na saída informar para ler do buffer (de 0) até o que foi lido em bytesRead (colocar em loop senão vai ler apenas 2048 e desprezar o resto)
+
+        }
+
+    } // esse transfer possui um código bem otimizado que quase não ocupa memória da aplicação e permite transferir grandes quantidades de dados, dependendo do cenário vale buscar um buffer ideal para a regra de negócio estabelecida
 }
